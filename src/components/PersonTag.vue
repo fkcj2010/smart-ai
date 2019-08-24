5ab4b82deb22f35b873b87f02466b00ef5842771
chenjie <744484220@qq.com>
2019-08-24 23:52:26 +0800

    merge

--- src/components/PersonTag.vue
+++ src/components/PersonTag.vue
@@ -12,21 +12,21 @@
         <div class="fs14">
             <el-row :span='24' class="item-row">
                 <el-col :span='4'>该用户困难指数:</el-col>
-                <el-col :span='2'>1</el-col>
+                <el-col :span='2'>{{stateHome.person.diffculty}}</el-col>
                 <el-col :span='10'>
                     <el-button type="mini" plain>社区帮助</el-button>
                 </el-col>
             </el-row>
             <el-row :span='24' class="item-row">
                 <el-col :span='4'>该用户信用评级:</el-col>
-                <el-col :span='2'>2</el-col>
+                <el-col :span='2'>{{stateHome.person.creditLevel}}</el-col>
                 <el-col :span='10'>
                     <el-button type="mini" plain>金融服务</el-button>
                 </el-col>
             </el-row>
             <el-row :span='24' class="item-row">
                 <el-col :span='4'>该用户能源消耗类别:</el-col>
-                <el-col :span='2' class="fs14">高消耗</el-col>
+                <el-col :span='2' class="fs14">{{stateHome.person.energyConsumption}}</el-col>
                 <el-col :span='10'>
                     <el-button type="mini" plain>缴费</el-button>
                     <el-button type="mini" plain>故障维修</el-button>
@@ -50,10 +50,13 @@ export default {
         }
     },
 
+    mounted() {
+        
+    },
+
     methods: {
         appraise() {
-            console.log(this.stateHome.person.name);
-            
+            this.$store.dispatch('home/getPerson', {name: this.stateHome.person.name})
         }
     },
 }
--- src/store/modules/home.js
+++ src/store/modules/home.js
@@ -1,20 +1,36 @@
-// initial state
-// shape: [{ id, quantity }]
+import axios from "axios";
+
 const state = {
     person: {
-        name: '123'
+        name: '123',
+        diffculty: '1',
+        creditLevel: '2',
+        energyConsumption: '3',
     }
     
 }
 
 // getters
 const getters = {
-  
+    
 }
 
 // actions
 const actions = {
-  
+    getPerson({state, commit}, payload) {
+        let {name} = payload
+        axios.get(`/smartSociety/a?name=${name}`).then(res => {
+            console.log(res);
+            let {diffculty, creditLevel, energyConsumption} = res.data
+            let person = {...state.person}
+            person.diffculty = diffculty
+            person.creditLevel = creditLevel
+            person.energyConsumption = energyConsumption
+            commit('setState', {
+                person
+            })
+        })
+    }
   
 }
 
